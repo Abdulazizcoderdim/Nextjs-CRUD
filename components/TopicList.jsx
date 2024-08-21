24b2e813b9ac4cbff8a9d687ae6d0d3bb452cde0
AbdulazizRus <coderdim@gmail.com>
2024-08-21 12:07:37 +0500

    changes

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -4,7 +4,9 @@ import RemoveBtn from './RemoveBtn'
 
 const getTopics = async () => {
   try {
-    const res = await fetch(process.env.API_GET, { cache: 'no-store' })
+    const res = await fetch('http://localhost:3000/api/topics', {
+      cache: 'no-store',
+    })
     if (!res.ok) {
       throw new Error('Failed to fetch topics')
     }
